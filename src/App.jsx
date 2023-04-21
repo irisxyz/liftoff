cee1cb7c468cc4518099d791e8598091e8d0afaf
unknown <ianmmah@gmail.com>
2023-04-20 18:52:03 -0700

    basic question tempalte

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const Center = styled.div`
 `
 
 const Centered = styled.div`
+  max-width: 700px;
 `
 
 const Slide1 = ({ onClick }) => {
@@ -21,7 +22,7 @@ const Slide1 = ({ onClick }) => {
     initial={{ opacity: 0 }}
     animate={{ opacity: 1 }}
     exit={{ opacity: 0 }}
-    transition={{ duration: 0.3 }}
+    transition={{ duration: 0.5 }}
   >
     <h1>gm! it&apos;s time to liftoff.</h1>
     <h1>8 questions.</h1>
@@ -31,6 +32,59 @@ const Slide1 = ({ onClick }) => {
   </m.div>
 }
 Slide1.propTypes = {
+  onClick: PropTypes.func
+}
+
+const Slide2 = ({ onClick }) => {
+  return <m.div
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
+    transition={{ duration: 0.5 }}
+  >
+    <h1>What should we call you?</h1>
+    novia
+    <br/>
+    <Button onClick={onClick}>Next</Button>
+  </m.div>
+}
+Slide2.propTypes = {
+  onClick: PropTypes.func
+}
+
+const Slide3 = ({ onClick }) => {
+  return <m.div
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
+    transition={{ duration: 0.5 }}
+  >
+    <h2>Welcome Novia! You are embarking on an expedition to discover life in space. Along this journey you make several key decisions, so choose carefully.
+    Time to take off for the stars for the adventure of a lifetime.
+    </h2>
+    <br/>
+    <Button onClick={onClick}>Next</Button>
+  </m.div>
+}
+Slide3.propTypes = {
+  onClick: PropTypes.func
+}
+
+const Slide4 = ({ onClick }) => {
+  return <m.div
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
+    transition={{ duration: 0.5 }}
+  >
+    <h2>Your primary objective is to investigate life in space life in space. What's your secret secondary objective?</h2>
+    <br/>
+    <Button onClick={onClick}>Collecting rare and mesmerizing rock and energy formations</Button>
+    <Button onClick={onClick}>Discover alternative sources of clean energy to bring back to earth </Button>
+    <Button onClick={onClick}>Mining valuable resources from asteroids</Button>
+  </m.div>
+}
+Slide4.propTypes = {
   onClick: PropTypes.func
 }
 
@@ -48,21 +102,35 @@ function App() {
   const pageDisplay = () => {
     switch(page) {
       case 0: 
-        return <Slide1 onClick={() => nextPage()}/>
+        return <Slide1 key="0" onClick={() => nextPage()}/>
+      case 1: 
+        return <Slide2 key="1" onClick={() => nextPage()}/>
+      case 2: 
+        return <Slide3 key="3" onClick={() => nextPage()}/>
+      case 3: 
+        return <Slide4 key="4" onClick={() => nextPage()}/>
+      case 4: 
+        return <Slide4 key="5" onClick={() => nextPage()}/>
+      case 5: 
+        return <Slide4 key="6" onClick={() => nextPage()}/>
     }
   }
 
   return (
-    <Center>
-      <Centered>
-        <Button onClick={() => prevPage()}>back</Button>
-        <Button onClick={() => nextPage()}>next</Button>
-        <AnimatePresence >
-        {page}
-        {pageDisplay()}
-        </AnimatePresence>
-      </Centered>
-    </Center>
+    <>
+      <div style={{position: 'absolute'}}>
+      <Button onClick={() => prevPage()}>back</Button>
+      {page}
+      <Button onClick={() => nextPage()}>next</Button>
+      </div>
+      <Center>
+        <Centered>
+          <AnimatePresence mode="wait">
+            {pageDisplay()}
+          </AnimatePresence>
+        </Centered>
+      </Center>
+    </>
   )
 }
 
