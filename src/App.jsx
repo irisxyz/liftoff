2e8962fed6d2cdde1a2c2015ec295c2633725464
unknown <ianmmah@gmail.com>
2023-04-20 18:00:13 -0700

    first animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { motion as m, AnimatePresence } from 'framer-motion'
 
 import Button from './components/Button'
 
@@ -14,23 +16,51 @@ const Center = styled.div`
 const Centered = styled.div`
 `
 
-const Slide1 = () => {
-  return <>
+const Slide1 = ({ onClick }) => {
+  return <m.div
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
+    transition={{ duration: 0.3 }}
+  >
     <h1>gm! it&apos;s time to liftoff.</h1>
     <h1>8 questions.</h1>
     <h1>3 minutes.</h1>
     <h1>Start your web3 journey here.</h1>
-    <Button>Start Quiz</Button>
-  </>
+    <Button onClick={onClick}>Start Quiz</Button>
+  </m.div>
+}
+Slide1.propTypes = {
+  onClick: PropTypes.func
 }
 
 function App() {
-  const [page, setCount] = useState(0)
+  const [page, setPage] = useState(0)
+
+  const prevPage = () => {
+    setPage(page - 1)
+  }
+
+  const nextPage = () => {
+    setPage(page + 1)
+  }
+
+  const pageDisplay = () => {
+    switch(page) {
+      case 0: 
+        return <Slide1 onClick={() => nextPage()}/>
+    }
+  }
 
   return (
     <Center>
       <Centered>
-        <Slide1/>
+        <Button onClick={() => prevPage()}>back</Button>
+        <Button onClick={() => nextPage()}>next</Button>
+        <AnimatePresence >
+        {page}
+        {pageDisplay()}
+        </AnimatePresence>
       </Centered>
     </Center>
   )
