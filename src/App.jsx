391811bf7cbef41521caa774b513a188a60230d8
ichung08 <isaacchung1990@gmail.com>
2023-04-23 17:30:36 -0700

    removed back button from home page

--- src/App.jsx
+++ src/App.jsx
@@ -81,7 +81,7 @@ function App() {
     if (page > 11) {
       setBg('#39375D')
     }
-    if (page >13) {
+    if (page > 13) {
       setBg('#160F66')
     }
   }, [page, setBg])
@@ -111,7 +111,7 @@ function App() {
         </Centered>
       </Center>
       <div style={{position: 'absolute', top: '1em', left: '1em' }}>
-      {page < 14 && <Button style={{ zIndex: 10 }} onClick={() => prevPage()}>&larr;</Button>}
+      {(page > 0 && page < 14) && <Button style={{ zIndex: 10 }} onClick={() => prevPage()}>&larr;</Button>}
       </div>
     </>
   )
